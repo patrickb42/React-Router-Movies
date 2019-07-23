c931ea092311b212cb20ff6aab3f5b1a9d22a737
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-23 00:10:51 -0400

    setup as jsx

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import SavedList from './Movies/SavedList';
+import Movie, { MovieType } from './Movies/Movie';
+
+const App = () => {
+  const [savedList, setSavedList] = useState( [] );
+
+  const addToSavedList = (movie: MovieType) => {
+    setSavedList( [...savedList, movie] );
+  };
+
+  return (
+    <Router>
+      <div>
+        <SavedList list={savedList} />
+        <div>Replace this Div with your Routes</div>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ client/src/Movies/Movie.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export interface MovieType {
+  id: number;
+  title: string;
+  director: string;
+  metascore: number;
+  stars: string[];
+}
+
+const Movie = () => {
+  const [movie, setMovie] = useState<MovieType | undefined>(undefined);
+
+  const axiosSetMovie = () => {
+    (async () => {
+      const id = 1;
+      // change ^^^ that line and grab the id from the URL
+      // You will NEED to add a dependency array to this effect hook
+
+      try {
+        const axiosResponse = await axios.get(`http://localhost:5000/api/movies/${id}`);
+        setMovie(axiosResponse.data);
+      } catch (e) {
+        console.error(e);
+      }
+      // axios
+      //   .get(`http://localhost:5000/api/movies/${id}`)
+      //   .then((response) => {
+      //     setMovie(response.data);
+      //   })
+      //   .catch((error) => {
+      //     console.error(error);
+      //   });
+    })();
+  };
+  useEffect(axiosSetMovie, []);
+
+  // Uncomment this only when you have moved on to the stretch goals
+  // const saveMovie = () => {
+  //   const addToSavedList = props.addToSavedList;
+  //   addToSavedList(movie)
+  // }
+
+  if (!movie) return <div>Loading movie information...</div>;
+
+  const { id, title, director, metascore, stars } = movie;
+  return (
+    <div className="save-wrapper">
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+      <div className="save-button">Save</div>
+    </div>
+  );
+};
+
+export default Movie;
